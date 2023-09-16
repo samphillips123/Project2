8236e4ec56887141f5d9ebf2aff6e8ddc05c7f18
samphillips123 <sphillips123@gmail.com>
2023-09-16 08:49:23 -0700

    updated model to have haterials as object in array

--- models/projects.js
+++ models/projects.js
@@ -1,29 +1,39 @@
 const mongoose = require('mongoose')
 
-// MATERIAL SCHEMA
-const materialSchema = new Schema ({
-    materialName: {type: String, required: true},
-    store: String,
-    estCost: {type: Number, required: true},
-    actCost: Number,
-    purchaseDate: {type: Date, min: '2000-01-01', max: today()},
-    trackingNum: String,
-    materialImg: String,
-    materialNotes: String,
-    materialComplete: Boolean
-})
+// // MATERIAL SCHEMA
+// const materialSchema = new mongoose.Schema ({
+//     materialName: {type: String, required: true},
+//     store: String,
+//     estCost: {type: Number, required: true},
+//     actCost: Number,
+//     purchaseDate: {type: Date, min: '2000-01-01'},
+//     trackingNum: String,
+//     materialImg: String,
+//     materialNotes: String,
+//     materialComplete: Boolean
+// })
 
 // PROJECT SCHEMA
 const projectSchema = new mongoose.Schema ({
     projectName: {type: String, require: true},
-    category: {type: Array, require: true, default: 'Home Improvement', 'Landscaping', 'Bicycle', 'Car', 'Personal'},
+    category: {type: String, require: true},
     description: String,
     projectComplete: Boolean,
     budget: {type: Number, required: true},
     estCostTtl: Number,
     actCostTtl: Number,
     projectImg: String,
-    materials: [materialSchema]
+    materials: [{
+        materialName: {type: String, required: true},
+        store: String,
+        estCost: {type: Number, required: true},
+        actCost: Number,
+        purchaseDate: {type: Date, min: '2000-01-01'},
+        trackingNum: String,
+        materialImg: String,
+        materialNotes: String,
+        materialComplete: Boolean
+    }]
 })
 
 // CREATE MODEL
