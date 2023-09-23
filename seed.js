73ccd37e0fb843791f39acf4eab6c39669e8ac10
samphillips123 <sphillips123@gmail.com>
2023-09-22 20:17:45 -0700

    updated seed.js

--- seed.js
+++ seed.js
@@ -48,7 +48,7 @@ Projects.create({
     ]
 },
 {
-    projectName: 'E30 BMW 3 Series Touring',
+    projectName: 'BMW E30 Touring',
     category: 'Cars',
     description: 'Import, restore, and modify old BMW station wagon to enjoy and potentially track at Laguna Seca Raceway.',
     projectComplete: false,
@@ -57,11 +57,11 @@ Projects.create({
     actCostTtl: 0,
     projectImg: '/imgs/bmw_wagon_project.jpeg',
     materials: [{
-            materialName: 'E30 BMW 3 Series Touring',
+            materialName: 'E30 BMW Wagon',
             store: 'TBD',
-            estCost: 5000,
+            estCost: 7500,
             materialImg: '/imgs/bmw_e30_touring.jpeg',
-            materialNotes: 'Need to have Richard help finding good condition wagon in Germany with his friend.',
+            materialNotes: 'Need to have Richard help finding good condition wagon in Germany with his friend. Years include 1988-1994',
             materialComplete: false
         },
         {
@@ -70,6 +70,52 @@ Projects.create({
             estCost: 2500,
             materialNotes: 'Shipping and import related items.',
             materialComplete: false
+        },
+        {
+            materialName: 'Motegi Wheels',
+            store: 'https://www.motegiracing.com/',
+            estCost: 100,
+            materialNotes: 'Not sure of style. At the moment, prefer bronze finish.',
+            materialComplete: false,
+            materialImg: '/imgs/motegi_wheels.jpeg'
+        }
+    ]
+},
+{
+    projectName: 'Front Lawn - Native Landscaping',
+    category: 'Home',
+    description: 'Remove grass lawn and replace with majority CA cative plants or local compatible plants.',
+    projectComplete: false,
+    budget: 4000,
+    projectImg: '/imgs/native_garden.JPG',
+    materials: [{
+            materialName: 'Bark Mulch',
+            store: 'Topes Landscaping Supplies',
+            estCost: 500,
+            materialNotes: 'Probably use mini-bark like the backyard but could use a combination with some gravel.',
+            materialComplete: false
+        },
+        {
+            materialName: 'Stepping Stone',
+            store: 'Topes Landscaping Supplies',
+            estCost: 200,
+            materialNotes: 'Stones to layout through garden.',
+            materialComplete: false
+        },
+        {
+            materialName: 'Native Plants',
+            store: 'Monterey County Native Plant Society',
+            estCost: 600,
+            materialNotes: 'Variety of native plants. Ex. hummingbird sage, lupine, sticky monkey, penstimon, Monterey Manzanitas...',
+            materialComplete: false
+        },
+        {
+            materialName: 'Landscape Design',
+            store: 'Either do it ourselves or higher a designer',
+            estCost: 150,
+            materialNotes: 'Map out and plan the layout of the landscaping.',
+            materialComplete: false,
+            materialImg: '/imgs/design_example.jpeg'
         }
     ]
 }).then((projects) => {
